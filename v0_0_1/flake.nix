7d239abc821a1288b6743016444f62147045f34d
hugosenari <hugosenari@gmail.com>
2022-03-23 19:07:34 -0300

    feat(nimpkgs): remove lock

--- v0_0_1/flake.nix
+++ v0_0_1/flake.nix
@@ -7,19 +7,19 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs.src-bingo-v0_0_1.flake = false;
-  inputs.src-bingo-v0_0_1.ref   = "refs/tags/v0.0.1";
-  inputs.src-bingo-v0_0_1.owner = "planetis-m";
-  inputs.src-bingo-v0_0_1.repo  = "bingo";
-  inputs.src-bingo-v0_0_1.type  = "github";
+  inputs.src-bingod-v0_0_1.flake = false;
+  inputs.src-bingod-v0_0_1.ref   = "refs/tags/v0.0.1";
+  inputs.src-bingod-v0_0_1.owner = "planetis-m";
+  inputs.src-bingod-v0_0_1.repo  = "bingo";
+  inputs.src-bingod-v0_0_1.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
-    args = ["self" "nixpkgs" "flakeNimbleLib" "src-bingo-v0_0_1"];
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-bingod-v0_0_1"];
   in lib.mkRefOutput {
     inherit self nixpkgs ;
-    src  = deps."src-bingo-v0_0_1";
+    src  = deps."src-bingod-v0_0_1";
     deps = builtins.removeAttrs deps args;
     meta = builtins.fromJSON (builtins.readFile ./meta.json);
   };
